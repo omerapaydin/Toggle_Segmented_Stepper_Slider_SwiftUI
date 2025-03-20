0cdf945e30311bb218a52b8d8d49af39399910b1
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-20 17:41:41 +0300

    Stepper

---
+++ Toggle_Segmented_Stepper_Slider_SwiftUI/Stepper.swift
@@ -0,0 +1,18 @@
+//
+//  Stepper.swift
+//  Toggle_Segmented_Stepper_Slider_SwiftUI
+//
+//  Created by Ömer on 20.03.2025.
+//
+
+import SwiftUI
+
+struct Stepper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Stepper()
+}
